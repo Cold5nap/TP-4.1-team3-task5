864c8f19a4eb39b2123449efa0bfc51d05a34b48
Андрей <and.andr2013@yandex.ru>
2022-04-19 17:20:13 +0300

    Убрал неиспользуемые файлы с переходом на rest api.

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('title')Про нас@endsection
-
-@section('content')
-    about
-@endsection
--- resources/views/inc/forms/add-to-cart.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="/shopping-cart" method="post">
-    @csrf{{--защищенный ключ--}}
-    <input type="hidden" name="goods_id" value="{{$item->id}}">
-    <button type="submit">Значок корзины</button>
-</form>
